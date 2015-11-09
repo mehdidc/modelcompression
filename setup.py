ceee51f87ecfd1e0f8986bf443a8b0f271e7edcf
mehdi <mehdi@cherti.name>
2015-11-09 16:37:04 +0100

    add initial files and setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="model_compression",
+    version="0.1",
+    author="mehdi cherti",
+    author_email="mehdi@cherti.name",
+    description=("modelcompression"),
+    license="BSD",
+    keywords="",
+    url="",
+    packages=["modelcompression"],
+)
